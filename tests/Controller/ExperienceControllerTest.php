9c9a51e5584606033d8d9950678ba23f798ab908
Anso <anso@gmail.com>
2024-05-01 13:57:47 +0200

    Develop initial structure for experience index page

--- tests/Controller/ExperienceControllerTest.php
+++ tests/Controller/ExperienceControllerTest.php
@@ -61,88 +61,88 @@ class ExperienceControllerTest extends WebTestCase
         self::assertSame(1, $this->repository->count([]));
     }
 
-    public function testShow(): void
-    {
-        $this->markTestIncomplete();
-        $fixture = new Experience();
-        $fixture->setTitle('My Title');
-        $fixture->setContent('My Title');
-        $fixture->setPlace('My Title');
-        $fixture->setDateOfExp('My Title');
-        $fixture->setDuration('My Title');
-        $fixture->setType('My Title');
-        $fixture->setTechnologies('My Title');
+    // public function testShow(): void
+    // {
+    //     $this->markTestIncomplete();
+    //     $fixture = new Experience();
+    //     $fixture->setTitle('My Title');
+    //     $fixture->setContent('My Title');
+    //     $fixture->setPlace('My Title');
+    //     $fixture->setDateOfExp('My Title');
+    //     $fixture->setDuration('My Title');
+    //     $fixture->setType('My Title');
+    //     $fixture->setTechnologies('My Title');
 
-        $this->manager->persist($fixture);
-        $this->manager->flush();
+    //     $this->manager->persist($fixture);
+    //     $this->manager->flush();
 
-        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+    //     $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
 
-        self::assertResponseStatusCodeSame(200);
-        self::assertPageTitleContains('Experience');
+    //     self::assertResponseStatusCodeSame(200);
+    //     self::assertPageTitleContains('Experience');
 
         // Use assertions to check that the properties are properly displayed.
     }
 
-    public function testEdit(): void
-    {
-        $this->markTestIncomplete();
-        $fixture = new Experience();
-        $fixture->setTitle('Value');
-        $fixture->setContent('Value');
-        $fixture->setPlace('Value');
-        $fixture->setDateOfExp('Value');
-        $fixture->setDuration('Value');
-        $fixture->setType('Value');
-        $fixture->setTechnologies('Value');
+    // public function testEdit(): void
+    // {
+    //     $this->markTestIncomplete();
+    //     $fixture = new Experience();
+    //     $fixture->setTitle('Value');
+    //     $fixture->setContent('Value');
+    //     $fixture->setPlace('Value');
+    //     $fixture->setDateOfExp('Value');
+    //     $fixture->setDuration('Value');
+    //     $fixture->setType('Value');
+    //     $fixture->setTechnologies('Value');
 
-        $this->manager->persist($fixture);
-        $this->manager->flush();
+    //     $this->manager->persist($fixture);
+    //     $this->manager->flush();
 
-        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+    //     $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
 
-        $this->client->submitForm('Update', [
-            'experience[title]' => 'Something New',
-            'experience[content]' => 'Something New',
-            'experience[place]' => 'Something New',
-            'experience[dateOfExp]' => 'Something New',
-            'experience[duration]' => 'Something New',
-            'experience[type]' => 'Something New',
-            'experience[technologies]' => 'Something New',
-        ]);
+    //     $this->client->submitForm('Update', [
+    //         'experience[title]' => 'Something New',
+    //         'experience[content]' => 'Something New',
+    //         'experience[place]' => 'Something New',
+    //         'experience[dateOfExp]' => 'Something New',
+    //         'experience[duration]' => 'Something New',
+    //         'experience[type]' => 'Something New',
+    //         'experience[technologies]' => 'Something New',
+    //     ]);
 
-        self::assertResponseRedirects('/experience/');
+    //     self::assertResponseRedirects('/experience/');
 
-        $fixture = $this->repository->findAll();
+    //     $fixture = $this->repository->findAll();
 
-        self::assertSame('Something New', $fixture[0]->getTitle());
-        self::assertSame('Something New', $fixture[0]->getContent());
-        self::assertSame('Something New', $fixture[0]->getPlace());
-        self::assertSame('Something New', $fixture[0]->getDateOfExp());
-        self::assertSame('Something New', $fixture[0]->getDuration());
-        self::assertSame('Something New', $fixture[0]->getType());
-        self::assertSame('Something New', $fixture[0]->getTechnologies());
-    }
+    //     self::assertSame('Something New', $fixture[0]->getTitle());
+    //     self::assertSame('Something New', $fixture[0]->getContent());
+    //     self::assertSame('Something New', $fixture[0]->getPlace());
+    //     self::assertSame('Something New', $fixture[0]->getDateOfExp());
+    //     self::assertSame('Something New', $fixture[0]->getDuration());
+    //     self::assertSame('Something New', $fixture[0]->getType());
+    //     self::assertSame('Something New', $fixture[0]->getTechnologies());
+    // }
 
-    public function testRemove(): void
-    {
-        $this->markTestIncomplete();
-        $fixture = new Experience();
-        $fixture->setTitle('Value');
-        $fixture->setContent('Value');
-        $fixture->setPlace('Value');
-        $fixture->setDateOfExp('Value');
-        $fixture->setDuration('Value');
-        $fixture->setType('Value');
-        $fixture->setTechnologies('Value');
+    // public function testRemove(): void
+    // {
+    //     $this->markTestIncomplete();
+    //     $fixture = new Experience();
+    //     $fixture->setTitle('Value');
+    //     $fixture->setContent('Value');
+    //     $fixture->setPlace('Value');
+    //     $fixture->setDateOfExp('Value');
+    //     $fixture->setDuration('Value');
+    //     $fixture->setType('Value');
+    //     $fixture->setTechnologies('Value');
 
-        $this->manager->persist($fixture);
-        $this->manager->flush();
+    //     $this->manager->persist($fixture);
+    //     $this->manager->flush();
 
-        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
-        $this->client->submitForm('Delete');
+    //     $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+    //     $this->client->submitForm('Delete');
 
-        self::assertResponseRedirects('/experience/');
-        self::assertSame(0, $this->repository->count([]));
-    }
-}
+    //     self::assertResponseRedirects('/experience/');
+    //     self::assertSame(0, $this->repository->count([]));
+    // }
+// }
